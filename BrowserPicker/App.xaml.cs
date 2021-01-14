9053ebede06221a5f1ea06864d10f3f07abd3730
Stuart Leeks <stuart@leeks.net>
2021-01-14 13:42:32 +0000

    whitepsace -> tabs

--- BrowserPicker/App.xaml.cs
+++ BrowserPicker/App.xaml.cs
@@ -26,31 +26,31 @@ namespace BrowserPicker
 			Deactivated += (sender, args) => ViewModel.OnDeactivated();
 		}
 
-        private string GetUnderlyingURL(string url)
-        {
-            if (url.StartsWith("https://staticsint.teams.cdn.office.net/evergreen-assets/safelinks/"))
-            {
+		private string GetUnderlyingURL(string url)
+		{
+			if (url.StartsWith("https://staticsint.teams.cdn.office.net/evergreen-assets/safelinks/"))
+			{
 				var uri = new Uri(url);
 				var queryString = uri.Query;
 				if (queryString[0] == '?')
-                {
+				{
 					queryString = queryString.Substring(1);
-                }
+				}
 				var queryStringElements = queryString.Split('&');
-                foreach (var queryStringElement in queryStringElements)
-                {
+				foreach (var queryStringElement in queryStringElements)
+				{
 					var parts = queryStringElement.Split('=');
 					if (parts.Length == 2 && parts[0] == "url")
-                    {
+					{
 						var underlyingUrl = HttpUtility.UrlDecode(parts[1]);
 						return underlyingUrl;
-                    }
-                }
-            }
+					}
+			}
+		}
 			return url;
 		}
 
-        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+		private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
 		{
 			var e = (Exception) unhandledExceptionEventArgs.ExceptionObject;
 			while(e != null)
