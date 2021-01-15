c56e85ae45dc553c146e60d5c4b05a57e9784013
Stuart Leeks <stuart@leeks.net>
2021-01-15 08:53:02 +0000

    Add prefix and regex rule types

--- BrowserPicker/DefaultSetting.cs
+++ BrowserPicker/DefaultSetting.cs
@@ -1,17 +1,30 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Windows.Media.Animation;
 using JetBrains.Annotations;
 
 namespace BrowserPicker
 {
 	public class DefaultSetting : INotifyPropertyChanged
 	{
+		private const string MatchType_Prefix = "prefix";
+		private const string MatchType_Regex = "regex";
+
 		public DefaultSetting(string fragment, string browser)
 		{
 			this.fragment = fragment;
 			this.browser = browser;
 		}
-
+		/// <summary>
+		/// If Fragment starts with a pipe (|) character then it is of the format
+		///      |type|value
+		/// Otherwise the Fragment is a suffix-match on the URL host name.
+		/// Currently supported Fragment types:
+		///      |prefix|https://example.com/test           This applies a prefix match to the full URL value
+		///      |regex|https://.*\.example\.com/test		This applies a regex match to the full URL value
+		/// </summary>
 		public string Fragment
 		{
 			get => fragment;
@@ -55,5 +68,33 @@ namespace BrowserPicker
 
 		private string fragment;
 		private string browser;
+
+		public int MatchLength(Uri url)
+		{
+			if (Fragment.StartsWith("|"))
+			{
+				var splitIndex = Fragment.IndexOf('|', 1);
+				if (splitIndex <0 )
+				{
+					// bad format
+					return 0;
+				}
+				var matchType = Fragment.Substring(1, splitIndex-1);
+				var value = Fragment.Substring(splitIndex+1);
+				switch (matchType)
+				{
+					case MatchType_Prefix:
+						return url.OriginalString.StartsWith(value) ? value.Length : 0;
+					case MatchType_Regex:
+						var match = Regex.Match(url.OriginalString, value);
+						return match.Length;
+					default:
+						// unhandled match type
+						return 0;
+				}
+			}
+			// Suffix match on the host name
+			return url.Host.EndsWith(Fragment) ? Fragment.Length : 0;
+		}
 	}
 }
