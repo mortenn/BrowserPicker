85ee4e98f393d03d4c1f952f08844591d33390ff
Morten Nilsen <morten@runsafe.no>
2025-03-25 20:57:40 +0100

    Add IoC support, move resource dictionary out of App.xaml

---
+++ src/BrowserPicker.App/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using BrowserPicker.View;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BrowserPicker;
+
+internal static class Program
+{
+	[STAThread]
+	private static void Main()
+	{
+		var host = Host.CreateDefaultBuilder()
+			.ConfigureServices(services =>
+			{
+				services.AddSingleton<App>();
+				services.AddSingleton<MainWindow>();
+			})
+			.Build();
+
+
+		var app = host.Services.GetRequiredService<App>();
+
+		var resourceDictionary = new ResourceDictionary
+		{
+			Source = new Uri(
+				"pack://application:,,,/BrowserPicker;component/Resources/ResourceDictionary.xaml",
+				UriKind.Absolute
+			)
+		};
+		app.Resources.MergedDictionaries.Add(resourceDictionary);
+
+		app.Run();
+	}
+}
