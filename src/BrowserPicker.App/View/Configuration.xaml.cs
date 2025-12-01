f7d919fa1af92d8e8a0f6f13bb48fab7c713e0db
XTsat <44708609+XTsat@users.noreply.github.com>
2025-12-01 10:11:13 +0800

    defaultapps

--- src/BrowserPicker.App/View/Configuration.xaml.cs
+++ src/BrowserPicker.App/View/Configuration.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace BrowserPicker.View;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace BrowserPicker.View;
 
 /// <summary>
 /// Interaction logic for Configuration.xaml
@@ -13,5 +17,13 @@ public partial class Configuration
 	private void CheckBox_Checked(object sender, System.Windows.RoutedEventArgs e)
 	{
 
+	}
+	private void OpenDefaultAppsSettings_Click(object sender, RoutedEventArgs e)
+	{
+		Process.Start(new ProcessStartInfo
+		{
+			FileName = "ms-settings:defaultapps",
+			UseShellExecute = true
+		});
 	}
 }
