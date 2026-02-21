5cff8c2899c09776ada3986ef37883b20d8e7805
Morten Nilsen <morten.nilsen@flytit.no>
2026-02-21 11:29:42 +0100

    Use a custom json converter for default settings, fixes #181

---
+++ src/BrowserPicker/DefaultSettingJsonConverter.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BrowserPicker;
+
+/// <summary>Enables System.Text.Json to deserialize <see cref="DefaultSetting"/> via its primary constructor.</summary>
+internal sealed class DefaultSettingJsonConverter : JsonConverter<DefaultSetting>
+{
+	public override DefaultSetting Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		MatchType type = default;
+		string? pattern = null;
+		string? browser = null;
+
+		if (reader.TokenType != JsonTokenType.StartObject)
+		{
+			throw new JsonException();
+		}
+
+		while (reader.Read())
+		{
+			if (reader.TokenType == JsonTokenType.EndObject)
+			{
+				break;
+			}
+			if (reader.TokenType != JsonTokenType.PropertyName)
+			{
+				continue;
+			}
+			var name = reader.GetString();
+			reader.Read();
+			switch (name)
+			{
+				case "Type":
+					type = JsonSerializer.Deserialize<MatchType>(ref reader, options);
+					break;
+				case "Pattern":
+					pattern = reader.GetString();
+					break;
+				case "Browser":
+					browser = reader.GetString();
+					break;
+				default:
+					reader.Skip();
+					break;
+			}
+		}
+
+		return new DefaultSetting(type, pattern, browser);
+	}
+
+	public override void Write(Utf8JsonWriter writer, DefaultSetting value, JsonSerializerOptions options)
+	{
+		writer.WriteStartObject();
+		writer.WriteString("Type", value.Type.ToString());
+		writer.WriteString("Pattern", value.Pattern);
+		writer.WriteString("Browser", value.Browser);
+		writer.WriteEndObject();
+	}
+}
